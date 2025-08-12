de55f2697c86943e90b717cff3dd0cdeb250f82c
wan <wan.lucas.90@gmail.com>
2025-08-12 17:40:22 -0300

    feat: add InputSanitizer module for input validation and sanitization

--- server/app/controllers/concerns/input_sanitizer.rb
+++ server/app/controllers/concerns/input_sanitizer.rb
@@ -12,4 +12,12 @@ module InputSanitizer
  
     sanitized.squeeze(' ').strip
   end
+
+  def valid_integer?(input, max: 100)
+    return false if input.blank?
+    return false unless input.match?(/\A\d+\z/)
+    
+    num = input.to_i
+    num > 0 && num <= max
+  end
 end
